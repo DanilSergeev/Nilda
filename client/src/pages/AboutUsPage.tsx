9d71edb94c2d09893ba1d8b78f449d78f2b35f2c
DanilSergeev <danil.sergeew5555@yandex.ru>
2024-09-10 23:17:38 +0300

    add aboutUsFB and create on client mailService

--- client/src/pages/AboutUsPage.tsx
+++ client/src/pages/AboutUsPage.tsx
@@ -1,12 +1,30 @@
+import CustomButton from '../components/GeneralComponents/button/CustomButton';
 import { Link } from 'react-router-dom';
+import AboutUsFeedBackComponents from '../components/AboutUsComponents/AboutUsFeedBackComponents';
 
 
 const AboutUsPage = () => {
     return (
 
         <main>
-           <h1></h1>
-            <p></p>
+            <section className='wrapper'>
+                <h1>Загаловок</h1>
+                <p>Под загаловок</p>
+                <CustomButton><a target="_blank" rel='noreferrer' href={`${process.env.REACT_APP_LINK_TO_BOOK}`} className='a-colorUnset'  >Предпросмотер книги</a></CustomButton>
+            </section>
+            <section className='abputUsSotial wrapper'>
+                <h2>Связь с автором</h2>
+                <p>Соц. сети</p>
+                <div>
+                    <Link to="/">sd</Link>
+                    <Link to="/">sd</Link>
+                    <Link to="/">sd</Link>
+                </div>
+            </section>
+            <AboutUsFeedBackComponents/>
+
+            {/* <section className='interactiveMap'></section>
+            На будущие */}
         </main>
     )
 }
