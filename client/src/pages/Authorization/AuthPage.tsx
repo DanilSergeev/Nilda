d3fec41eef01102a8ae7456305557720954af4ef
DanilSergeev <danil.sergeew5555@yandex.ru>
2024-10-18 18:16:45 +0300

    updata auth register pages

--- client/src/pages/Authorization/AuthPage.tsx
+++ client/src/pages/Authorization/AuthPage.tsx
@@ -3,10 +3,15 @@ import Form from 'react-bootstrap/Form';
 import CustomButton from '../../components/GeneralComponents/button/CustomButton';
 import { Link } from 'react-router-dom';
 import CommonLine from '../../components/GeneralComponents/line/CommonLine';
+import { useState } from 'react';
 
 
 
 const AuthPage = () => {
+    const [dataAuth, setDataAuth] = useState({
+        login: '',
+        password: '',
+    })
 
     return (
         <>
@@ -17,12 +22,22 @@ const AuthPage = () => {
                     <Form className='wrapper mt-5'>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Введите логин</Form.Label>
-                            <Form.Control type="login" placeholder="Enter login" />
+                            <Form.Control
+                                type="login"
+                                placeholder="Введите логин"
+                                value={dataAuth.login}
+                                onChange={e => setDataAuth(prev => ({ ...prev, login: e.target.value }))}
+                            />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Введите пароль</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Введите пароль"
+                                value={dataAuth.password}
+                                onChange={e => setDataAuth(prev => ({ ...prev, password: e.target.value }))}
+                            />
                         </Form.Group>
 
                         <CustomButton variant="primary" >
--- client/src/pages/Authorization/RegisterPage.tsx
+++ client/src/pages/Authorization/RegisterPage.tsx
@@ -3,10 +3,17 @@ import Form from 'react-bootstrap/Form';
 import CustomButton from '../../components/GeneralComponents/button/CustomButton';
 import { Link } from 'react-router-dom';
 import CommonLine from '../../components/GeneralComponents/line/CommonLine';
+import { useState } from 'react';
 
 
 
 const RegisterPage = () => {
+    const [dataAuth, setDataAuth] = useState({
+        login: '',
+        password: '',
+        rePassword: ''
+    })
+
 
     return (
         <>
@@ -16,17 +23,32 @@ const RegisterPage = () => {
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Введите логин</Form.Label>
-                            <Form.Control type="login" placeholder="Enter login" />
+                            <Form.Control
+                                type="login"
+                                value={dataAuth.login}
+                                onChange={e => setDataAuth(prev => ({ ...prev, login: e.target.value }))}
+                                placeholder="Введите логин"
+                            />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Введите пароль</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Введите пароль"
+                                value={dataAuth.password}
+                                onChange={e => setDataAuth(prev => ({ ...prev, password: e.target.value }))}
+                            />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Повторите пароль</Form.Label>
-                            <Form.Control type="password" placeholder="Repeat the password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Повторите пароль"
+                                value={dataAuth.rePassword}
+                                onChange={e => setDataAuth(prev => ({ ...prev, rePassword: e.target.value }))}
+                            />
                         </Form.Group>
 
                         <CustomButton variant="primary" >
